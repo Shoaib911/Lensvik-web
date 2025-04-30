f71252ae2b8d6b4e1d5df0098e2ef680263102c3
Shoaib Altaf <shoaibaltaf200@gmail.com>
2025-04-30 23:13:43 +0500

    test

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -186,7 +186,7 @@ const onResults = (results) => {
     const frameWidth = faceWidth * 1.25;
 
     const isMobile = detectMobile(); // Use updated helper
-    const frameHeight = frameWidth * (isMobile ? 0.4 : 0.65);
+    const frameHeight = frameWidth * (isMobile ? 0.35 : 0.65);
 
     // ✅ Adjust alignment offsets only for mobile
     const offsetY = isMobile ? -frameHeight *0.08 : -15; // vertical nudge down
